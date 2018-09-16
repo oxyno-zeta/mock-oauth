1927b255ca97e22bad9cc32bee974dfa4e6aff20
shoubhik bose <sbose78@gmail.com>
2018-09-16 23:55:07 +0530

    fix username format

--- main.go
+++ main.go
@@ -1,60 +1,58 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "net/url"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
+	"net/url"
 )
 
 var (
-    message string
-    address string
+	message string
+	address string
 )
-
 
 func handleAuth(w http.ResponseWriter, r *http.Request) {
 
-
-	whereToRedirect := r.Referer()
+	whereToRedirect := r.URL.Query().Get("redirect_uri")
 	if whereToRedirect == "" {
 		whereToRedirect = "http://example.org?no=1234"
 	}
 	redirectURL, _ := url.Parse(whereToRedirect)
-	redirectURL.Query().Set("state", r.URL.Query().Get("state"))
-	redirectURL.Query().Set("code", "randomcode")
+	params := redirectURL.Query()
+	params.Set("state", r.URL.Query().Get("state"))
+	params.Set("code", "randomcode")
+
+	redirectURL.RawQuery = params.Encode()
 
 	w.Header().Set("Location", redirectURL.String())
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func handleToken(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("{\"token\":\"sometoken\"}"))
 }
 
-
-
-func handleUserInfo(w http.ResponseWriter, r *http.Request){
-    w.Header().Set("Content-Type", "application/json")
-    w.Write([]byte("{\"name\":\"johndoe\",\"email\":\"a@a.com\",\"username\":\"someusername\"}"))
+func handleUserInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"name\":\"johndoe\",\"email\":\"a@a.com\",\"preferred_username\":\"shbose\",\"sub\":\"62ccaef-5bd6-48c8-bbb1-987d39482a02\"}"))
 }
-
 
 func init() {
-    flag.StringVar(&message, "message", "there", "a message to print")
-    flag.StringVar(&address, "address", "0.0.0.0:8080", "address/port to listen on")
+	flag.StringVar(&message, "message", "there", "a message to print")
+	flag.StringVar(&address, "address", "0.0.0.0:8080", "address/port to listen on")
 }
 func main() {
-    flag.Parse()
-    http.HandleFunc("/auth", handleAuth)
+	flag.Parse()
+	http.HandleFunc("/auth", handleAuth)
 	http.HandleFunc("/token", handleToken)
-	http.HandleFunc("/userinfo",handleUserInfo)
+	http.HandleFunc("/userinfo", handleUserInfo)
 	/*
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s\n", message)
-    })
-    */
-    log.Printf("listening on %s...", address)
-    log.Fatal(http.ListenAndServe(address, nil))
+		    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello %s\n", message)
+		    })
+	*/
+	log.Printf("listening on %s...", address)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
